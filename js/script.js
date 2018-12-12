ec2c5eb04b5ec61bc4be4cde125565945f15f69b
basiacz <basiaczerw@gmail.com>
2018-12-12 13:29:10 +0100

    10.3

--- js/script.js
+++ js/script.js
@@ -5,23 +5,41 @@ Mustache.parse(templateCarousel);
 
 for (var i = 0; i < slideShow.length; i++) {
     elem.insertAdjacentHTML('beforeEnd', Mustache.render(templateCarousel, slideShow[i]));
-}
+};
 
 var flkty = new Flickity(elem, {
     cellAlign: 'left',
     contain: true,
-    hash: true,
+    //hash: true,
 });
 
-var restartButton = document.querySelector('.button');
+var restartButton = document.querySelector('button');
 
 restartButton.addEventListener('click', function () {
     flkty.select(0);
 });
 
-var progressBar = document.querySelector('.progress-bar');
+var progressBar = document.querySelector('.progress-bar')
 
 flkty.on('scroll', function (progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
+
+window.initMap = function () {
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 15,
+            center: slideShow[0].coords
+        }
+    );
+
+    var markers = [];
+
+    for (let j = 0; j < slideShow.length; j++) {
+        markers.push(new google.maps.Marker({
+            position: slideShow[j].coords,
+            map: map
+        }));
+    }
+};
